e5a2c829b67be1472f1e9a41fe9c6bce49211547
Constantin ARMAND <constantin.armand@gmail.com>
2021-11-30 12:00:39 +0100

    Test Spawn text via JS

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let emplacementHistoire = "Dans le QG des Scorpion Buster";
+let TitreHistoire ="Début de l'aventure";
+let TexteHistoire ="Les Scorpion Buster sont actuellement entrain de manger des croissant et des pain au chocolat offert par leur dernier client .Un peu plus tard dans la journée il reçoivent un appel d'un potentiel client , apparement une infastion de scorpion est apparue dans sont garage . Les scorpion Buster se prépare a y aller , mais comment vont-ils se rendre sur place ? :";
+let templatestory = `<div class="card">
+<h5 class="card-header">${TitreHistoire}</h5>
+<div class="card-body">
+  <h5 class="card-title">${emplacementHistoire}</h5>
+  <p class="card-text">${TexteHistoire}</p>
+</div>
+</div>`
+
+let mainBody = document.getElementById("Main");
+
+function dotheThing()
+{
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML=templatestory;
+    mainBody.appendChild(newDiv);
+}
