1a1c6a7bf2c2885d01d8bdbb62cc73880d481ad2
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2021-11-30 17:00:01 +0100

    js

--- js/index.js
+++ js/index.js
@@ -9,12 +9,51 @@ let templatestory = `
   <p class="card-text">${TexteHistoire}</p>
 </div>
 </div>`;
-
+let c1a = "Je lui propose d'aller dans un bar";
+let c1b = "Kill the kid";
+let c1c = "I help the kid in order to heal him";
+let r1a = "You take the kid to the bar. It limps along next to you, and you carry it over your shoulder like a sack of potatoes. You enter the rundown building and find a ransacked bar."
+let r1b = "You pick the kid up with one arm and stab it in the chest with your knife. The kid dies in your arms and you take it's body, slinging it over your shoulders and continuing your search for food. "
+let r1c =  "You find a rusty old first aid kit and a big medical needle. You use the needle to remove a sliver of metal from the mutant's foot."
 let mainBody = document.getElementById("Main");
 
-function dotheThing()
+function dotheThing(response)
 {
     let newDiv = document.createElement("div");
-    newDiv.innerHTML=templatestory;
-    mainBody.appendChild(newDiv);
+    newDiv.innerHTML=response;
+    mainBody.append(newDiv);
+}
+
+function history(histoire) {
+  let newDiv = document.createElement("div");
+  newDiv.innerHTML= histoire;
+  mainBody.append(newDiv);
+}
+
+function question(length, a, b, c, ra, rb, rc) {
+  history(templatestory)
+  let newbutton1 = document.createElement("button");
+  let newbutton2 = document.createElement("button");
+  let newbutton3 = document.createElement("button");
+  newbutton1.addEventListener('click', ()=> {
+    dotheThing(ra)
+  })
+  newbutton2.addEventListener('click', ()=> {
+    dotheThing(rb)
+  })
+  newbutton3.addEventListener('click', ()=> {
+    dotheThing(rc)
+  })
+  newbutton1.innerHTML = a;
+  newbutton2.innerHTML = b;
+  newbutton3.innerHTML = c;
+  if (length == 2){
+    mainBody.append(newbutton1);
+    mainBody.append(newbutton2);
+  } else if (length == 3) {
+    mainBody.append(newbutton1);
+    mainBody.append(newbutton2);
+    mainBody.append(newbutton3);
+  }
+
 }
