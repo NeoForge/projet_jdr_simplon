71d519efc981a15edd3c9a986d01b94321e5e648
Constantin ARMAND <constantin.armand@gmail.com>
2021-12-01 17:41:07 +0100

    Up

--- js/index2.js
+++ js/index2.js
@@ -18,6 +18,7 @@ let LastChoiceTwo;
 let LastChoiceThree;
 let waiting = true;
 
+let choiceArray = [] ;
 
 
 
@@ -28,7 +29,6 @@ fetchInfo().then(WaitForStart());
 
 
 async function fetchInfo() {
-  waiting=true;
   fetch('../js/story.json')
     .then(response => response.json())
     .then(data => DataStoryJson = data)
@@ -39,68 +39,93 @@ async function fetchInfo() {
     .catch(error => console.log(error));
 }
 
-function WaitForStart()
-{
-  setTimeout(() => {  Start() }, 150);
+function WaitForStart() {
+  setTimeout(() => { Start() }, 150);
 }
 
 function Start() {
-  
+
   let FinalString;
-  if (indexStory > 0) {
-    FinalString = DataQuestionJson[LastQuestionId].resultat + " " + DataStoryJson[indexStory].body
+
+  if (indexStory == Object.keys(DataStoryJson).length) {
+    ChoiceOne.parentElement.classList.add("disabled");
+    ChoiceTwo.parentElement.classList.add("disabled");
+    ChoiceThree.parentElement.classList.add("disabled");
   }
   else {
-    FinalString = DataStoryJson[indexStory].body
-  }
-  TitleCard.innerHTML = DataStoryJson[indexStory].title;
-  StoryCard.innerHTML = FinalString;
-  QuestionCard.innerHTML = DataStoryJson[indexStory].question;
-  for (let i = 0; i < Object.keys(DataQuestionJson).length; i++) {
-    if (DataQuestionJson[i].storyid == indexStory) {
-      switch (DataQuestionJson[i].choiceid) {
-        case 1:
-          {
-            ChoiceOne.innerHTML = DataQuestionJson[i].body;
-            LastChoiceOne = i;
-            break;
-          }
-        case 2:
-          {
-            ChoiceTwo.innerHTML = DataQuestionJson[i].body;
-            LastChoiceTwo = i;
-            break;
-          }
-        case 3:
-          {
-            ChoiceThree.innerHTML = DataQuestionJson[i].body;
-            LastChoiceThree = i;
-            break;
-          }
+    if (indexStory > 0) {
+      FinalString = DataQuestionJson[LastQuestionId].resultat + " " + DataStoryJson[indexStory].body
+    }
+    else {
+      FinalString = DataStoryJson[indexStory].body
+    }
+    TitleCard.innerHTML = DataStoryJson[indexStory].title;
+    StoryCard.innerHTML = FinalString;
+    QuestionCard.innerHTML = DataStoryJson[indexStory].question;
+    for (let i = 0; i < Object.keys(DataQuestionJson).length; i++) {
+      if (DataQuestionJson[i].storyid == indexStory) {
+        switch (DataQuestionJson[i].choiceid) {
+          case 1:
+            {
+              ChoiceOne.innerHTML = DataQuestionJson[i].body;
+              LastChoiceOne = i;
+              break;
+            }
+          case 2:
+            {
+              ChoiceTwo.innerHTML = DataQuestionJson[i].body;
+              LastChoiceTwo = i;
+              break;
+            }
+          case 3:
+            {
+              ChoiceThree.innerHTML = DataQuestionJson[i].body;
+              LastChoiceThree = i;
+              break;
+            }
+        }
       }
     }
   }
 }
 
-function clickButton(int) {
-  console.log("Ping " + int);
-  switch (int) {
-    case 1:
-      {
-        LastQuestionId = LastChoiceOne;
-        break;
-      }
-    case 2:
-      {
-        LastQuestionId = LastChoiceTwo;
-        break;
-      }
-    case 3:
-      {
-        LastQuestionId = LastChoiceThree;
-        break;
-      }
+function EndGame()
+{
+  let EndDiv = document.createElement("div");
+  EndDiv.innerHTML = DataStoryJson[0].body;
+  for(let i =0;i<choiceArray.length;i++)
+  {
+    let tempHtml = EndDiv.innerHTML;
+    EndDiv.innerHTML = EndDiv.innerHTML +" "+DataQuestionJson[choiceArray[i]].resultat+" "+DataStoryJson[i].body
   }
-  indexStory++;
-  Start();
+  mainBody.innerHTML = "";
+  mainBody.appendChild(EndDiv);
+}
+
+
+function clickButton(int) {
+  if (ChoiceOne.parentElement.classList.contains("disabled")) { EndGame();}
+  else {
+    switch (int) {
+      case 1:
+        {
+          LastQuestionId = LastChoiceOne;
+          break;
+        }
+      case 2:
+        {
+          LastQuestionId = LastChoiceTwo;
+          break;
+        }
+      case 3:
+        {
+          LastQuestionId = LastChoiceThree;
+          break;
+        }
+    }
+    choiceArray.push(LastQuestionId);
+    indexStory++;
+    Start();
+  }
+
 }
